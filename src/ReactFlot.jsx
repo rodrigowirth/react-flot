afb4b4883dc7d9ad8e211b1829cad3059c37e109
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2016-10-14 13:30:45 -0300

    Make charts responsive

--- src/ReactFlot.jsx
+++ src/ReactFlot.jsx
@@ -2,18 +2,37 @@ import $ from 'jquery';
 import React, { Component, PropTypes } from 'react';
 
 import '../flot/jquery.flot';
+// import '../flot/jquery.flot.resize';
 import '../flot/jquery.flot.tooltip';
 
 class ReactFlot extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
-    data: PropTypes.isRequired,
-    options: PropTypes.isRequired,
+    data: PropTypes.array.isRequired,
+    options: PropTypes.object.isRequired,
     height: PropTypes.string,
     width: PropTypes.string,
   }
 
   componentDidMount() {
+    this.draw();
+
+    if (window.attachEvent) {
+      window.attachEvent('onresize', this.draw.bind(this));
+    } else if (window.addEventListener) {
+      window.addEventListener('resize', this.draw.bind(this), true);
+    }
+  }
+
+  componentWillUnmount() {
+    if (window.attachEvent) {
+      window.detachEvent('onresize', this.draw.bind(this));
+    } else if (window.addEventListener) {
+      window.removeEventListener('resize', this.draw.bind(this), true);
+    }
+  }
+
+  draw() {
     $.plot($(`#${this.props.id}`), this.props.data, this.props.options);
   }
 
